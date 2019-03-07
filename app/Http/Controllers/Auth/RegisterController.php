a19495c834de2067586617127f00a9aae8e9a3bb
juandagalo <juandagarcial@gmail.com>
2019-03-06 19:04:30 -0500

    Organizacion cerrar sesión
    Se organiza el botón de cerrar sesión

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,6 @@ class RegisterController extends Controller
 
     protected function create(array $data)
     {
-        return "hola";
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -34,27 +34,26 @@
 	      </button>
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-						<li class="nav-item active"><a href="index.html" class="nav-link pl-0">Inicio</a></li>
-						<li class="nav-item"><a href="#" class="nav-link">Descarga</a></li>
+				<li class="nav-item active"><a href="index.html" class="nav-link pl-0">Inicio</a></li>
+				<li class="nav-item"><a href="#" class="nav-link">Descarga</a></li>
 	        	<li class="nav-item"><a href="#" class="nav-link">Acerca de nosotros</a></li>
-						<li class="nav-item"><a href="#" class="nav-link">Contáctanos</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Contáctanos</a></li>
+                @if (Auth::check())
+                {{csrf_field()}}
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <form action="{{route('logout')}}" method="POST">
+                            {{csrf_field()}}
+                            <button type="submit">cerrar</button>
+                        </form>
+                    </a>
+                </li>
+                @endif
 	        </ul>
 	      </div>
 	    </div>
 	  </nav>
     <!-- END nav -->
-
-
-
-    <!--<h1>bienvenido {{ auth()->user()->name}}</h1>
-
-	<form action="{{route('logout')}}" method="POST">
-		{{csrf_field()}}
-		<button type="submit">cerrar</button>
-
-	</form>-->
-
-
 
     <section class="home-slider owl-carousel">
       <div class="slider-item" style="background-image:url(images/banner.jpg);">
