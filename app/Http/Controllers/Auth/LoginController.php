1e4fb334f6003241f296c58cae38739c0046dc3b
Juan Daniel Garcia Lopez <31321197+juandagalo@users.noreply.github.com>
2019-04-02 13:33:51 -0500

    Corecciones

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function login(){
+    public function login()
+    {
        $credentials = $this->validate(request(),['email'=>'email|required|string','password'=>'required|string']);
 
         if (Auth::attempt($credentials)) {
@@ -20,7 +21,8 @@ class LoginController extends Controller
         }
     }
 
-    public function logout(){
+    public function logout()
+    {
         Auth::logout();
         return redirect('/');
     }
