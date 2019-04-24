d2777d93416a46fc1ecaed5d7bb2bc6c7b5feaeb
luisloupb <lflt96@hotmail.com>
2019-04-23 22:27:43 -0500

    Implementación vista recomendaciones
    Actualización de archivos css y js.

---
+++ resources/views/recommend.blade.php
@@ -0,0 +1,53 @@
+@extends("layouts.template")
+
+@section("content")
+<div class="container">
+    <div class="row">
+        <div class="col-md-12" id="cajon-texto" style="background-color: white;">
+            <h3 style="text-align: center; padding: 30px; font-weight: bold">Estas son nuestras recomendaciones</h3>
+            <table class="table table-bordered">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col"></th>
+                        <th scope="col">Canción</th>
+                        <th scope="col">Artista</th>
+                        <th scope="col">Álbum</th>
+                    </tr>
+                    </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
