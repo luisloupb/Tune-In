1070d992fa85b6f9e3f22e6bc4882dd00e639742
JOSEBRANDL <jbrandlondoo@yahoo.es>
2019-05-14 22:06:34 -0500

    cambio en el localresearch blade

--- resources/views/localResearch.blade.php
+++ resources/views/localResearch.blade.php
@@ -3,11 +3,11 @@
 @section("content")
 <div class="container">
 	<div>
-		 <a href="{{ route('tutorial') }}" class="btn btn-warning btn-lg">Ir a tutorial</a>
+		 <a href="{{ route('tutorial') }}" class="btn btn-warning btn-lg" style="margin: 20px;">Ir a tutorial</a>
 	</div>
 	<div>
-		<input type="file" accept="audio/*" multiple/>
-		<input type="Button" value="Enviar" id="enviar" name="" onclick="enviar();">
+		<input type="file" accept="audio/*" multiple style="margin: 20px;"/>
+		<input type="Button" value="Enviar" id="enviar" name="" onclick="enviar();" style="margin: 20px;">
 		<div id="vista" style="background-color: white;"></div>
 	</div>
 </div>
