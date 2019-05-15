00e5fa8cd8d831f6daff84abed6f094ae01f96c4
FelipeFranco97 <francozuluaga97@gmail.com>
2019-05-14 21:44:58 -0500

    Update localResearch.blade.php
    Primer intento :v

--- resources/views/localResearch.blade.php
+++ resources/views/localResearch.blade.php
@@ -1,14 +1,15 @@
 @extends("layouts.template")
 
 @section("content")
-<div>
-	 <a href="{{ route('tutorial') }}" class="btn btn-warning btn-lg">ir a tutorial</a>
-</div>
-<div>
-	<input type="file" accept="audio/*" multiple/>
-	<input type="Button" value="enviar" id="enviar" name="" onclick="enviar();">
-	<input type="Button" value="verdatos" id="BtnVerDatos" name="" onclick="verdatos();">
-	<div id="vista" style="background-color: white;"></div>
+<div class="container">
+	<div>
+		 <a href="{{ route('tutorial') }}" class="btn btn-warning btn-lg">Ir a tutorial</a>
+	</div>
+	<div>
+		<input type="file" accept="audio/*" multiple/>
+		<input type="Button" value="Enviar" id="enviar" name="" onclick="enviar();">
+		<div id="vista" style="background-color: white;"></div>
+	</div>
 </div>
 
 @endsection
